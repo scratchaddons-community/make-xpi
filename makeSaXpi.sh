1a62db5e895feddd5b1464c0dacb4865138dede5
Shock59 <42130427+shock59@users.noreply.github.com>
2022-01-13 11:43:22 +1100

    Add files via upload

---
+++ makeSaXpi.sh
@@ -0,0 +1,13 @@
+if test -d "ScratchAddons"; then
+    echo "Please remove the ScratchAddons directory or move to another directory!"
+    exit
+fi
+git clone https://github.com/ScratchAddons/ScratchAddons.git
+if test -f "ScratchAddons.xpi"; then
+    rm ScratchAddons.xpi
+fi
+cd ScratchAddons
+zip -r ../ScratchAddons.xpi ./*
+cd ../
+rm -r -f ScratchAddons
+echo "ScratchAddons.xpi has been created."
